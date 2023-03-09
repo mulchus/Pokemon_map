42271c4c0e7f9b9f9651773f571ed2aa27851462
Юрий <mulch@list.ru>
2023-03-09 15:53:11 +0300

    maded dis_appeared filtrations by __lte __gte - dont work - see removed HttpResponseNotFound

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,6 +1,5 @@
 import folium
 
-from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from .models import Pokemon, PokemonEntity
 from datetime import datetime
@@ -31,15 +30,14 @@ def show_all_pokemons(request):
     pokemons = Pokemon.objects.all()
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     now = datetime.now().timestamp()
-    pokemon_entities = PokemonEntity.objects.all()
+    pokemon_entities = PokemonEntity.objects.filter(appeared_at__lte=now, disappeared_at__gte=now)
     for pokemon_entity in pokemon_entities:
-        if pokemon_entity.appeared_at.timestamp() <= now <= pokemon_entity.disappeared_at.timestamp():
-            add_pokemon(
-                folium_map,
-                pokemon_entity.lat,
-                pokemon_entity.lon,
-                request.build_absolute_uri(f'/media/{pokemon_entity.pokemon.photo}')
-            )
+        add_pokemon(
+            folium_map,
+            pokemon_entity.lat,
+            pokemon_entity.lon,
+            request.build_absolute_uri(f'/media/{pokemon_entity.pokemon.photo}')
+        )
 
     pokemons_on_page = []
     for pokemon in pokemons:
@@ -56,53 +54,52 @@ def show_all_pokemons(request):
 
 def show_pokemon(request, pokemon_id):
     pokemon = Pokemon.objects.get(id=pokemon_id)
-    now = datetime.now().timestamp()
+    now = datetime.now()
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    pokemon_entities = pokemon.pokemon_entity.all()
+    pokemon_entities = pokemon.pokemon_entity.filter(appeared_at__lte=now, disappeared_at__gte=now)
     pokemons_description = {}
     for pokemon_entity in pokemon_entities:
-        if pokemon_entity.appeared_at.timestamp() <= now <= pokemon_entity.disappeared_at.timestamp():
-            photo = request.build_absolute_uri(f'/media/{pokemon.photo}')
-            add_pokemon(
-                folium_map,
-                pokemon_entity.lat,
-                pokemon_entity.lon,
-                photo,
-            )
+        photo = request.build_absolute_uri(f'/media/{pokemon.photo}')
+        add_pokemon(
+            folium_map,
+            pokemon_entity.lat,
+            pokemon_entity.lon,
+            photo,
+        )
 
-            pokemons_description = {
-                'pokemon_id': pokemon.id,
-                'title_ru': pokemon.title,
-                'title_en': pokemon.title_en,
-                'title_jp': pokemon.title_jp,
-                'description': pokemon.description,
-                'img_url': request.build_absolute_uri(f'/media/{pokemon.photo}'),
-                'entities': {
-                    'level': pokemon_entity.level,
-                    'lat': pokemon_entity.lat,
-                    'lon': pokemon_entity.lon
+        pokemons_description = {
+            'pokemon_id': pokemon.id,
+            'title_ru': pokemon.title,
+            'title_en': pokemon.title_en,
+            'title_jp': pokemon.title_jp,
+            'description': pokemon.description,
+            'img_url': request.build_absolute_uri(f'/media/{pokemon.photo}'),
+            'entities': {
+                'level': pokemon_entity.level,
+                'lat': pokemon_entity.lat,
+                'lon': pokemon_entity.lon
+            },
+        }
+        if pokemon.previous_pokemon.first():
+            next_pokemon = pokemon.previous_pokemon.first()
+            pokemons_description.update({
+                'next_evolution': {
+                    'title_ru': next_pokemon.title,
+                    'pokemon_id': next_pokemon.id,
+                    'img_url': request.build_absolute_uri(f'/media/{next_pokemon.photo}')
                 },
-            }
-            if pokemon.previous_pokemon.first():
-                next_pokemon = pokemon.previous_pokemon.first()
-                pokemons_description.update({
-                    'next_evolution': {
-                        'title_ru': next_pokemon.title,
-                        'pokemon_id': next_pokemon.id,
-                        'img_url': request.build_absolute_uri(f'/media/{next_pokemon.photo}')
-                    },
-                })
-            if pokemon.previous_evolution:
-                pokemons_description.update({
-                    'previous_evolution': {
-                        'title_ru': pokemon.previous_evolution.title,
-                        'pokemon_id': pokemon.previous_evolution.id,
-                        'img_url': request.build_absolute_uri(f'/media/{pokemon.previous_evolution.photo}')
-                    },
-                })
+            })
+        if pokemon.previous_evolution:
+            pokemons_description.update({
+                'previous_evolution': {
+                    'title_ru': pokemon.previous_evolution.title,
+                    'pokemon_id': pokemon.previous_evolution.id,
+                    'img_url': request.build_absolute_uri(f'/media/{pokemon.previous_evolution.photo}')
+                },
+            })
 
-        else:
-            return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
+        # else:
+        #     return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
 
     return render(request, 'pokemon.html', context={
         'map': folium_map._repr_html_(),
